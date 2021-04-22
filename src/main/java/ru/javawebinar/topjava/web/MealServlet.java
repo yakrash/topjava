81aeea42612de82a0650aa83bbacd4389d6d95df
yakrash <kraash@bk.ru>
2021-04-22 17:48:20 +0300

    HW 05 - 4+

--- src/main/java/ru/javawebinar/topjava/web/MealServlet.java
+++ src/main/java/ru/javawebinar/topjava/web/MealServlet.java
@@ -28,6 +28,8 @@ public class MealServlet extends HttpServlet {
     @Override
     public void init() {
         springContext = new ClassPathXmlApplicationContext("spring/spring-app.xml", "spring/spring-db.xml");
+        springContext.getEnvironment().setActiveProfiles("postgres", "datajpa");
+        springContext.refresh();
         mealController = springContext.getBean(MealRestController.class);
     }
 
