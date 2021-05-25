e178453926a45aea022280b6afbf381f24fbf38c
yakrash <kraash@bk.ru>
2021-05-25 12:01:34 +0300

    patch 806

--- src/main/java/ru/javawebinar/topjava/model/User.java
+++ src/main/java/ru/javawebinar/topjava/model/User.java
@@ -148,6 +148,10 @@ public class User extends AbstractNamedEntity {
         return meals;
     }
 
+    public void setMeals(List<Meal> meals) {
+        this.meals = meals;
+    }
+
     @Override
     public String toString() {
         return "User{" +
