5553d6c6fdbca16b64511fe7e68ce3e8a0ed3217
yakrash <kraash@bk.ru>
2021-05-26 13:09:35 +0300

    HW_08 - 1

---
+++ src/main/webapp/resources/js/topjava.meals.js
@@ -0,0 +1,40 @@
+const mealAjaxUrl = "ui/meals/";
+
+// https://stackoverflow.com/a/5064235/548473
+const ctx = {
+    ajaxUrl: mealAjaxUrl
+};
+
+$(function () {
+    makeEditable(
+        $("#datatable").DataTable({
+            "paging": false,
+            "info": true,
+            "columns": [
+                {
+                    "data": "dateTime"
+                },
+                {
+                    "data": "description"
+                },
+                {
+                    "data": "calories"
+                },
+                {
+                    "defaultContent": "Edit",
+                    "orderable": false
+                },
+                {
+                    "defaultContent": "Delete",
+                    "orderable": false
+                }
+            ],
+            "order": [
+                [
+                    0,
+                    "asc"
+                ]
+            ]
+        })
+    );
+});
